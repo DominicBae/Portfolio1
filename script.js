fc75ef94d0ce198bb6f4ba8c76945155dd13b579
DominicBae <baehope823@hanmail.net>
2024-10-10 09:23:44 +0900

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
+// window scroll
+$(window).on("scroll", function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop >= 80) {
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header');
+    }
+});
+
+//   Document Ready
+$(document).ready(function() {
+    // Typing animation
+    new Typed('#type-it', {
+        strings: ['소통을 통해 새로운 가치를 창출하는', '사용자의 목소리를 코드로 실현하는', '사용자의 편리함을 최우선으로 설계하는'],
+        typeSpeed: 100,
+        loop: true
+      });
+});
