889aa1682e2f1f67532801350c0976bf3a665998
DominicBae <baehope823@hanmail.net>
2024-10-17 10:59:55 +0900

    about2

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(window).on("scroll", function () {
 $(document).ready(function() {
     // Typing animation
     new Typed('#type-it', {
-        strings: ['소통을 통해 사용자 중심의 웹을 만드는', '사용자의 목소리를 코드로 실현하는', '사용자 편의를 최우선으로 설계하는'],
+        strings: ['소통으로 사용자 중심 웹을 만드는', '사용자의 목소리를 코드로 실현하는', '사용자 편의를 최우선으로 설계하는'],
         typeSpeed: 100,
         loop: true
       });
